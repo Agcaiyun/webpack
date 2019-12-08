bf66dad474093ef3f7b88e824ae12d9b02b3410a
agcaiyun <agcaiyun@163.com>
2019-12-08 13:59:28 +0000

    add file-loader

--- webpack-kaikeba/index.js
+++ webpack-kaikeba/index.js
@@ -1,6 +1,17 @@
 import a from './a.js'
 import b from './b.js'
 
+import img from './img/first.jpg'
+
+console.log(img)
+
+var imgSrc = img
+var rootDOM = document.getElementById('root')
+var image = new Image()
+
+image.src = imgSrc
+rootDOM.appendChild(image)
+
 a()
 b()
 
--- webpack-kaikeba/webpack.config.js
+++ webpack-kaikeba/webpack.config.js
@@ -3,7 +3,21 @@ const path = require("path");
 module.exports = {
 	"entry": "./index.js",
 	"output": {
-		path: path.resolve(__dirname, "./build"),
-		filename: "index.js"
+		path: path.resolve(__dirname, "./dist"),
+		filename: "main.js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jpg$/,
+				use: {
+					loader: 'file-loader',
+					options: {
+						name: '[name].[ext]',
+						outputPath: 'img'
+					}
+				}
+			}
+		]
 	}
 } 	

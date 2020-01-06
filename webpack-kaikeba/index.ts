597c290c95b2a69995ac6a81d60dbe929dd4753d
agcaiyun <agcaiyun@163.com>
2020-01-06 19:16:53 +0800

    update index.ts

--- webpack-kaikeba/index.ts
+++ webpack-kaikeba/index.ts
@@ -1,51 +1,72 @@
-interface Person {
-    firstName: string,
-    lastName: string
-}
+// interface Person {
+//     firstName: string,
+//     lastName: string
+// }
 
-interface Options {
-    color?: string;
-    volume?: number
-}
+// interface Options {
+//     color?: string;
+//     volume?: number
+// }
 
-enum Color {
-    Red = 1,
-    Green,
-    Bule
-}
+// enum Color {
+//     Red = 1,
+//     Green,
+//     Bule
+// }
 
-let c: Color = Color.Green
-let cc: string = Color[2]
-console.log(c, cc, Color[0])
+// let c: Color = Color.Green
+// let cc: string = Color[2]
+// console.log(c, cc, Color[0])
 
-function greeter(person: Person) {
-    return `Hello, ${person.firstName} - ${person.lastName}`
-}
+// function greeter(person: Person) {
+//     return `Hello, ${person.firstName} - ${person.lastName}`
+// }
 
-function foo() {
-    console.log(`a -- ${a}`)
-}
+// function foo() {
+//     console.log(`a -- ${a}`)
+// }
 
-foo()
+// foo()
 
-let a
+// let a
 
-function test() {
-    console.log(bbb)
-}
-test()
+// function test() {
+//     console.log(bbb)
+// }
+// test()
 
-let bbb
+// let bbb
 
-let user = {
-    firstName: 'firstName',
-    lastName: 'lastName'
-}
+// let user = {
+//     firstName: 'firstName',
+//     lastName: 'lastName'
+// }
 
-let options: Options = {}
-options.color = 'red'
-options.volume = 11
+// let options: Options = {}
+// options.color = 'red'
+// options.volume = 11
 
-document.body.innerHTML = greeter(user)
+// document.body.innerHTML = greeter(user)
 
-export default greeter
+// export default greeter
+
+
+// let o = {
+//     a: "foo",
+//     b: 12,
+//     c: "bar"
+// };
+
+// let { a: newName1, b: newName2 }: { a: string, b: number } = o
+
+// interface LabelledValue {
+//     label: string;
+//     size?: number;
+//   }
+
+//   function printLabel(labelledObj: LabelledValue) {
+//     console.log(labelledObj.label, labelledObj.size);
+//   }
+
+//   let myObj = {size: 10, label: "Size 10 Object"};
+//   printLabel(myObj);

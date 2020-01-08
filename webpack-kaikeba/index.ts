a48cdb04eff519a14be650fa4245d6f82ade5edf
agcaiyun <agcaiyun@163.com>
2020-01-08 11:24:03 +0800

    set this's type

--- webpack-kaikeba/index.ts
+++ webpack-kaikeba/index.ts
@@ -1,14 +1,24 @@
-var suits = ["hearts", "spades", "clubs", "diamonds"]
+interface Card {
+    suit: string;
+    card: number;
+}
 
-let deck = {
+interface Deck {
+    suits: Array<string>;
+    cards: Array<number>;
+    createCardPicker(this: Deck): () => Card;
+}
+
+let deck: Deck = {
     suits: ["hearts", "spades", "clubs", "diamonds"],
     cards: Array(52),
-    createCardPicker: function () {
-        return function () {
+    createCardPicker: function (this: Deck) {
+        // NOTE: the line below is now an arrow function, allowing us to capture 'this' right here
+        return () => {
             let pickedCard = Math.floor(Math.random() * 52);
             let pickedSuit = Math.floor(pickedCard / 13);
 
-            return { suit: this.suits[3] };
+            return { suit: this.suits[pickedSuit], card: pickedCard % 13 };
         }
     }
 }
@@ -16,4 +26,4 @@ let deck = {
 let cardPicker = deck.createCardPicker();
 let pickedCard = cardPicker();
 
-alert("card: " + " of " + pickedCard.suit);
+alert("card: " + pickedCard.card + " of " + pickedCard.suit);

a6d04b37ee8c1c6b532be5af919cecb7b7436281
agcaiyun <agcaiyun@163.com>
2020-01-09 13:28:58 +0800

    User-Defined Type Guards

--- webpack-kaikeba/index.ts
+++ webpack-kaikeba/index.ts
@@ -1,6 +1,27 @@
-enum Status { Ready, Waiting };
-enum Color { Red, Blue, Green };
+interface Bird {
+    fly();
+    layEggs();
+}
 
-let test: Status.Ready = Status.Ready;
-test = 111;
-test = Status.Waiting;  
+interface Fish {
+    swim();
+    layEggs();
+}
+
+function getSmallPet(): Fish | Bird {
+    return
+}
+
+function isFish(pet: Fish | Bird): pet is Fish {
+    return (<Fish>pet).swim !== undefined
+}
+
+let pet = getSmallPet();
+
+if (isFish(pet)) {
+    pet.swim()
+} else {
+    pet.fly()
+}
+
+
